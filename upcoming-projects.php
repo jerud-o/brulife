aba94fbaa2c7ba10750e31ad64db0513d82040a2
Jake Lunaria Flores <90809353+Tsokolatee@users.noreply.github.com>
2022-07-13 17:29:36 +0800

    Upcoming Projects
    Added Upcoming Projects Webpage

---
+++ upcoming-projects.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;600;700;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d9b0f2969c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/upcoming-projects.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Upcoming Projects</title>
+</head>
+
+<body>
+
+    <!-- LIST FILTER -->
+
+
+    <div class="m-10">
+        <div class="w-[80%] m-auto ">
+            <ul class="filter-list-custom">
+                <li><a href="#">Show All</a></li>
+                <li><a href="#">Subdivision</a></li>
+                <li><a href="#">Business Buidling</a></li>
+                <li><a href="#">Hospital</a></li>
+                <li><a href="#">Malls</a></li>
+                <li><a href="#">Industrial</a></li>
+                <li><a href="#">Agricultural</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+    <!-- 1ST ROW CARDS -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/001.png" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Dressing and Processing Plant</h3>
+                <p>Industrial</p>
+                <button class="btn-custom">See More</button>
+            </div>
+
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/002.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>BRU Life Twin Tower Business Bldg.</h3>
+                <p>Business Building</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/003.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>The Platform Mall</h3>
+                <p>Malls</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 2ND ROW CARDS -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/004.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Klaires Ville Subdivision</h3>
+                <p>Subdivision</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/005.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>New Hope Medical Center</h3>
+                <p>Hospital</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/006.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>BRU Life IP Tower</h3>
+                <p>Business Building</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- 3RD ROW CARDS -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/007.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>New Hope Medical Center</h3>
+                <p>Hospital</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/008.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Ricefield</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/009.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Water and Sewage Treatment</h3>
+                <p>Industrial</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- 4TH ROW CARDS -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/010.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Cattle</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/011.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Goat Farming</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/012.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Winery</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- 5TH ROW CARD -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/013.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Poultry</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/014.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Man-Made Lake</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/015.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Fruit-Bearing Tress and Vegetables</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- 6TH ROW CARD -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/016.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Power Plant</h3>
+                <p>Power Plant</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/017.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Cornfield</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/018.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Piggery</h3>
+                <p>Agricultural</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- 7TH ROW CARD -->
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-custom-img">
+                <img src="assets/img/upcoming-projects-img/019.jpg" alt="demo">
+            </div>
+            <div class="card-custom-content">
+                <h3>Feed Milling</h3>
+                <p>Industrial</p>
+                <button class="btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="h-[50vh]"></div>
+
+    <!-- END -->
+</body>
+
+</html>
+
+
