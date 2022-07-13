858597214650e19af19518063694a164ee69be71
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-07-13 21:41:55 +0800

    careers-page

---
+++ careers.php
@@ -0,0 +1,42 @@
+<?php require_once "includes/header.php"; ?>
+<?php require_once "includes/navbar.php"; ?>
+</head>
+
+<body class="career">
+  <div class="-scale-x-100 bg-[url(assets/img/career-bg.jpg)] bg-cover bg-no-repeat blur-[8px] h-full absolute w-full z-[-1] top-20"></div>
+  <div class="w-2/3 py-8 px-12 mx-auto bg-[rgba(200,200,200,.0)]">
+    <h1 class="text-center text-5xl font-bold mb-4">Welcome to Our Career Center</h1>
+    <p class="text-center indent-10">On this page you will find information and resources about working at Brulife. There are exciting job opportunities and if you are looking for additional insights about our job functions, want to get to know the people of Brulife and out culture, or to get professional insights on your career, check out our Careers blog page for more.</p>
+  </div>
+  <div class="wrapper bg-primary-green w-1/2 px-12 mx-auto mb-20 pt-10">
+    <h2 class="text-5xl text-center text-white">Jobs & Training</h2>
+    <div class="flex justify-around py-12">
+      <ul>
+        <li class="text-white">➤ Skilled Workers</li>
+        <li class="text-white">➤ Engineering</li>
+        <li class="text-white">➤ Architectural</li>
+        <li class="text-white">➤ Designer</li>
+        <li class="text-white">➤ Project Managers</li>
+        <li class="text-white">➤ Inspector</li>
+        <li class="text-white">➤ Interior Design</li>
+      </ul>
+      <ul>
+        <li class="text-white">➤ Agriculture</li>
+        <li class="text-white">➤ Marine</li>
+        <li class="text-white">➤ Medical Education</li>
+        <li class="text-white">➤ Trading</li>
+        <li class="text-white">➤ Management</li>
+        <li class="text-white">➤ HRM</li>
+        <li class="text-white">➤ Real Estate</li>
+      </ul>
+    </div>
+
+  </div>
+  
+  <?php
+  require_once "includes/footer.php";
+  require_once "includes/scripts.php";
+  ?>
+</body>
+
+</html>
