2c2e1201370d555881572cd5d908bf57f8016aa4
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-07-14 12:51:44 +0800

    partners(aemm)

---
+++ aemm.php
@@ -0,0 +1,69 @@
+<?php require_once "includes/header.php"; ?>
+<?php require_once "includes/navbar.php"; ?>
+</head>
+
+<body>
+  <div class="flex items-center justify-center gap-12 py-12 bg-primary-green mx-auto my-12">
+    <div class="basis-1/4 text-6xl">
+      <h1 class="text-white">AEMM GENCON ENGINEERING SERVICES</h1>
+    </div>
+
+    <div class="">
+      <img class="mx-auto" src="assets/img/aemm.jpg">
+    </div>
+  </div>
+  <div class="text-white w-4/5 flex flex-shrink-0 justify-center gap-12 p-12 mx-auto bg-primary-green">
+   
+    <!-- About Company -->
+    <div class="about">
+      <div class="section mb-4">
+        <h1 class="text-3xl font-medium mb-4">ABOUT Gencon Engineering Services</h1>
+        <p class="paragraph">AEMM GENCON ENGINEERING SERVICES is a newly engineering/construction firm and trading company created this year 2021. Our company is involved in design and build of Residential House, Building Condominium, Mall, Warehouses, Roads, Land Development, Industrial, Waste to Energy, Windmill, Solar Power, Hydro, Oil and Gas, Petrochemical and other projects related to civil/structural, Mechanical, Electrical and Telecommunication.</p>
+      </div>
+
+      <div class="section mb-4">
+        <h1 class="sub-header font-medium mb-2">Our Mission</h1>
+        <p class="paragraph">To create more jobs and to establish a big difference in terms of construction, maintenance and operation. And to support the humanitarian and restoration of natural resources in terms of agricultural farming and animal farming industry and to support our food industry to control and maintain the price or cost of the food for the nation.</p>
+      </div>
+
+      <div class="section mb-4">
+        <h1 class="sub-header font-medium mb-2">Our Vision</h1>
+        <p class="paragraph">Taking care of the interests of our customers and manufacturing companies which we consider as our business partners and providing the highest quality, service and products besides catching our own growth.</p>
+      </div>
+
+      <div class="section mb-4">
+        <h1 class="sub-header font-medium mb-2">Our Core Values</h1>
+        <p class="paragraph">We at AEMM believe that quality of works, products and services are foremost concerns to satisfy our customers. Therefore we strive for excellence in everything that we do, and we value most in our works.</p>
+      </div>
+    </div>
+     <!-- Company Contact -->
+    <div class="contact">
+      <h1 class="text-3xl font-medium">Contact Us</h1>
+      <div class="mt-6 flex">
+        <span class="text-slate-200">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+          </svg>
+        </span>
+        <p class="ml-4 text-white">0955-285-6155</p>
+      </div>
+      <div class="flex mt-6">
+        <span class="text-slate-200">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+          </svg>
+        </span>
+        <a href="mailto: aemm.ges@gmail.com" class="underline ml-4 cursor-pointer text-white">aemm.ges@gmail.com</a>
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+</svg></button>
+      </div>
+    </div>
+  </div>
+  <?php
+  require_once "includes/footer.php";
+  require_once "includes/scripts.php";
+  ?>
+  <script src="assets/js/aemm.js"></script>
+</body>
+
+</html>
