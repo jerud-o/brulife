61483022e891c8ee65f1016bb794a512335eb43a
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-07-15 15:49:40 +0800

    aci(start)

---
+++ aci.php
@@ -0,0 +1,21 @@
+<?php require_once "includes/header.php"; ?>
+<?php require_once "includes/navbar.php"; ?>
+<div class="mb-12 my-8">
+   <img src="assets/img/aci.jpg" alt="aci-banner" class="mx-auto w-full">
+</div>
+<div class="partners-about">
+   <div>
+      <h1 class=" text-5xl font-bold">About</h1>
+   </div>
+   <div>
+   
+   </div>
+</div>
+<?php
+require_once "includes/footer.php";
+require_once "includes/scripts.php";
+?>
+
+</body>
+
+</html>
