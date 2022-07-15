6b74fe815418456044172bba092e8c7e78db7377
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-07-15 10:55:22 +0800

    brucgt

---
+++ brucgt.php
@@ -0,0 +1,65 @@
+<?php require_once "includes/header.php"; ?>
+<?php require_once "includes/navbar.php"; ?>
+</head>
+
+<body>
+  <div class="flex items-center justify-center p-12 gap-12 bg-primary-green mx-auto my-12">
+    <div class="basis-1/4 text-6xl">
+      <h1 class="text-white">BRU Consumer Goods Trading</h1>
+    </div>
+
+    <div class="basis-3/4">
+      <img class="mx-auto border-slate-300 rounded-md border-[16px]" src="assets/img/brucgt.jpg">
+    </div>
+  </div>
+  <div class="text-white w-4/5 flex flex-shrink-0 justify-center gap-12 p-12 mx-auto bg-primary-green">
+    <!-- About Company -->
+    <div class="about">
+      <h1 class="text-4xl font-medium mb-4">About Us</h1>
+      <div class="section mb-4">
+        <h1 class="sub-header font-medium mb-4">Who Are We</h1>
+        <p class="paragraph">BRU Consumer Goods Trading provides best credentials for wants, needs, and supplying demand into one place that gives consumer satisfaction.</p>
+      </div>
+
+      <div class="section mb-4">
+        <h1 class="sub-header font-medium mb-2">Our Mission</h1>
+        <p class="paragraph">To be your trusted partner by giving friendly price without compromising the satisfaction, quality of our product, and the core values of our company to our customer. Conveniency is our priority.</p>
+      </div>
+
+      <div class="section mb-4">
+        <h1 class="sub-header font-medium mb-2">Our Vision</h1>
+        <p class="paragraph">To be your trusted partner by giving friendly price without compromising the satisfaction, quality of our product, and the core values of our company to our customer. Conveniency is our priority.</p>
+      </div>
+    </div>
+    <!-- Company Contact -->
+    <div class="contact">
+      <h1 class="text-4xl font-medium">Contact Us</h1>
+      <div class="mt-6 flex">
+        <span class="text-slate-200">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+          </svg>
+        </span>
+        <p class="ml-4 text-white">+63285600830 / +639272586797</p>
+      </div>
+      <div class="flex mt-6">
+          <span class="text-slate-200 inline ">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+          </span>
+
+        <a href="brucgt.com" class="underline ml-4 cursor-pointer text-white">www.brucgt.com</a>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+        </svg></button>
+      </div>
+    </div>
+  </div>
+  <?php
+  require_once "includes/footer.php";
+  require_once "includes/scripts.php";
+  ?>
+  <script src="assets/js/aemm.js"></script>
+</body>
+
+</html>
