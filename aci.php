8b103ebc6738c549e786832752a8e8c84f694b6a
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-07-16 21:51:19 +0800

    aci-update

--- aci.php
+++ aci.php
@@ -1,14 +1,25 @@
 <?php require_once "includes/header.php"; ?>
 <?php require_once "includes/navbar.php"; ?>
-<div class="py-12 bg-primary-gray">
+<div class="bg-[url(assets/img/engineers.jpg)] bg-no-repeat bg-cover fixed top-0 blur-[3px] h-[110%] w-full z-[-1]"></div>
+<div class="py-12">
    <img src="assets/img/aci.jpg" alt="aci-banner" class="mx-auto w-full">
 </div>
-<div class="text-white w-4/5 flex flex-shrink-0 p-12 mx-auto bg-primary-green">
+<div class="text-white w-3/5 flex flex-col flex-shrink-0 p-12 mx-auto bg-primary-green">
    <div>
       <h1 class=" text-5xl font-bold mb-4">About</h1>
-      <p class="indent-16 leading-7 mb-3 capitalize">A group of engineers, while working abroad, dreamt of becoming one of the leading contractors for civil and electro-mechanical construction.</p>
-      <p class="indent-16 leading-7 mb-3 capitalize">A head-start was on 2010, the construction of Rochester Residences under Empire East, the two-storey residential building at Moonwalk village, Las Piñas City and the four-storey residential building in Makati City. From there on, projects in various areas of vertical and horizontal infrastructure started to rise, from warehouses, road infrastructures to industrial plants.</p>
-      <p class="indent-16 leading-7 mb-3 capitalize">ACI maintain its honesty, integrity, reliability and technical competence. Our target is to complete the projects at its best while enduring setback and challenges. As we move into the future, ACI is better prepared now than ever before. We are ready to take on thatever challenge tha come out way without losing sight of its vision; all of us at ACI remain committed to excellence and growth.</p>
+      <p class="paragraph">A group of engineers, while working abroad, dreamt of becoming one of the leading contractors for civil and electro-mechanical construction.</p>
+      <p class="paragraph">A head-start was on 2010, the construction of Rochester Residences under Empire East, the two-storey residential building at Moonwalk village, Las Piñas City and the four-storey residential building in Makati City. From there on, projects in various areas of vertical and horizontal infrastructure started to rise, from warehouses, road infrastructures to industrial plants.</p>
+      <p class="paragraph">ACI maintain its honesty, integrity, reliability and technical competence. Our target is to complete the projects at its best while enduring setback and challenges. As we move into the future, ACI is better prepared now than ever before. We are ready to take on thatever challenge tha come out way without losing sight of its vision; all of us at ACI remain committed to excellence and growth.</p>
+   </div>
+   <div>
+      <h1 class="sub-header">Mission</h1>
+      <p class="paragraph">The company continues to move forward to become the leading engineering and construction firm in the Philippines by consistently delivering cost-effective projects that meet international standards.</p>
+   </div>
+
+   <div>
+      <h1 class="sub-header">Vision</h1>
+      <p class="paragraph">Our mission is to undertake the engineering construction business with a focus on becoming the cost leader with excellence in quality performance and on time delivery, while at the same time mindful of safety and environmental concerns.</p>
+      <p class="paragraph">With our philosophy, values, and mission firmly in place, we are targeting controlled growth and sustained profitability. Our responsibility to our clients, our human resource, and shareholders remain to be the priority of our business and is defined by our corporate values.</p>
    </div>
    <div>
       
@@ -19,11 +30,11 @@ require_once "includes/footer.php";
 require_once "includes/scripts.php";
 ?>
 <script>
-   document.body.style.backgroundImage ="url(assets/img/engineers.jpg)";
-   document.body.style.backgroundRepeat = "no-repeat";
-   document.body.style.backgroundSize = "cover";
-   document.body.style.backgroundAttachment = "fixed";
-
+   // document.body.style.backgroundImage = "url(assets/img/engineers.jpg)";
+   // document.body.style.backgroundRepeat = "no-repeat";
+   // document.body.style.backgroundSize = "cover";
+   // document.body.style.backgroundAttachment = "fixed";
+   document.body.style.background = "transparent";
 </script>
 </body>
 
