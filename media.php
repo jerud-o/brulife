596a045c04f113aff81fea25cfa55a4a97af8894
Jake Lunaria Flores <90809353+Tsokolatee@users.noreply.github.com>
2022-07-18 17:32:40 +0800

    media and up-pro
    added responsive and some new modification in the web page of media and entertainment and also in up-pro

---
+++ media.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/d9b0f2969c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/media.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Entertainment and Media</title>
+</head>
+
+<body>
+
+    <div class="custom-content-wrapper">
+        <div class="custom-bg"></div>
+        <div class="custom-content">
+            <div class="custom-header">
+                <img class="custom-logo" src="assets/img/entmedia-img/brutv-logo.png" alt="bru-logo">
+                <div class="custom-title">BRU Digital TV</div>
+            </div>
+            <div class="custom-text-content">
+                <h1>About Us</h1>
+                <p>BRU Digital Television is a broadcasting and media corporation duly registered to SEC, airing news, business, lifestyles and entertainment shows in 9 social media platforms anchored by professionals and business executives.</p>
+            </div>
+        </div>
+        <button class="custom-button-style custom-button" href="https://www.youtube.com/channel/UCfZ4kY2ve7yy7u0v5hCOOwA" target="_blank">Visit Youtube Channel</button>
+    </div>
+
+    <div class="custom-caption">
+        <h1>Uploaded Videos</h1>
+    </div>
+
+    <div class="custom-video-layout">
+        <iframe class="custom-video-style" src="https://www.youtube.com/embed/8rkaIOzaCKM?modestbranding=1&amp;rel=0&amp;autoplay=1&amp;mute=1&amp;loop=1" title="BRUTV SIGNING ON" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="custom-video-style" src="https://www.youtube.com/embed/_eJbV6MyegQ?modestbranding=1&amp;rel=0" title="E-GAME TODAY - Teaser" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="custom-video-style" src="https://www.youtube.com/embed/BnPYt1y1w_w?modestbranding=1&amp;rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <div class="h-[20vh]"></div>
+
+</body>
+
+</html>
---
+++ up-pro.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;600;700;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d9b0f2969c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/up-pro.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Upcoming Projects</title>
+</head>
+
+<body>
+
+    <div class="m-10">
+        <div class="w-[80%] lg:w-[97%] xl:w-[80%] m-auto">
+            <ul class="filter-list-custom">
+                <li><a href="#">Show All</a></li>
+                <li><a href="#">Subdivision</a></li>
+                <li><a href="#">Business Buidling</a></li>
+                <li><a href="#">Hospital</a></li>
+                <li><a href="#">Malls</a></li>
+                <li><a href="#">Industrial</a></li>
+                <li><a href="#">Agricultural</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/001.png" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>DRESSING AND PROCESSING PLANT</h3>
+                <p>INDUSTRIAL</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/002.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>BRU LIFE TWIN TOWER BUSINESS CENTER</h3>
+                <p>BUSINESS BLDG.</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/003.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>THE PLATFORM MALL</h3>
+                <p>MALLS</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/004.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>KLAIRES VILLE SUBDIVISION</h3>
+                <p>SUBDIVISION</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/005.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>NEW HOPE MEDICAL CENTRE</h3>
+                <p>HOSPITALS</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/006.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>BRU LIFE IP TOWER</h3>
+                <p>BUSINESS BLDG.</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/007.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>NEW HOPE MEDICAL CENTRE</h3>
+                <p>HOSPITALS</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/008.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>RICEFIELD</h3>
+                <p>AGRICULTURAL</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-flex-custom">
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/009.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>WATER AND WATER SEWAGE TREATMENT</h3>
+                <p>INDUSTRIAL</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+
+        <div class="card-custom">
+            <div class="card-img-custom">
+                <img src="assets/img/upcomingproj-img/010.jpg" alt="img">
+            </div>
+            <div class="relative card-content-custom">
+                <h3>CATTLE</h3>
+                <p>AGRICULTURAL</p>
+                <button class="absolute btn-custom">See More</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
