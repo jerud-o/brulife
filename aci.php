3739085c6e024586892e0628f4ac2f5ac109e763
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-07-18 13:55:26 +0800

    aci(made responsive)

--- aci.php
+++ aci.php
@@ -1,28 +1,73 @@
 <?php require_once "includes/header.php"; ?>
 <?php require_once "includes/navbar.php"; ?>
 <div class="bg-[url(assets/img/engineers.jpg)] bg-no-repeat bg-cover fixed top-0 blur-[3px] h-[110%] w-full z-[-1]"></div>
-<div class="py-12">
-   <img src="assets/img/aci.jpg" alt="aci-banner" class="mx-auto w-full">
+<div class="h-32 mb-4 lg:py-12 lg:h-72 xl:h-80">
+   <img src="assets/img/aci.jpg" alt="aci-banner" class="h-full w-full">
 </div>
-<div class="text-white w-3/5 flex flex-col flex-shrink-0 p-12 mx-auto bg-primary-green">
-   <div>
-      <h1 class=" text-5xl font-bold mb-4">About</h1>
+<div class="text-white rounded-md w-11/12 md:w-10/12 md:p-8 lg:w-4/6 p-4 mx-auto bg-primary-green mb-12">
+   <div class="mb-10">
+      <h1 class=" text-4xl font-bold mb-4">About</h1>
       <p class="paragraph">A group of engineers, while working abroad, dreamt of becoming one of the leading contractors for civil and electro-mechanical construction.</p>
       <p class="paragraph">A head-start was on 2010, the construction of Rochester Residences under Empire East, the two-storey residential building at Moonwalk village, Las Piñas City and the four-storey residential building in Makati City. From there on, projects in various areas of vertical and horizontal infrastructure started to rise, from warehouses, road infrastructures to industrial plants.</p>
-      <p class="paragraph">ACI maintain its honesty, integrity, reliability and technical competence. Our target is to complete the projects at its best while enduring setback and challenges. As we move into the future, ACI is better prepared now than ever before. We are ready to take on thatever challenge tha come out way without losing sight of its vision; all of us at ACI remain committed to excellence and growth.</p>
+      <p class="paragraph">ACI maintains its honesty, integrity, reliability and technical competence. Our target is to complete the projects at its best while enduring setback and challenges. As we move into the future, ACI is better prepared now than ever before. We are ready to take on thatever challenge tha come out way without losing sight of its vision; all of us at ACI remain committed to excellence and growth.</p>
    </div>
-   <div>
+   <div class="mb-4">
       <h1 class="sub-header">Mission</h1>
       <p class="paragraph">The company continues to move forward to become the leading engineering and construction firm in the Philippines by consistently delivering cost-effective projects that meet international standards.</p>
    </div>
 
-   <div>
+   <div class="mb-4">
       <h1 class="sub-header">Vision</h1>
       <p class="paragraph">Our mission is to undertake the engineering construction business with a focus on becoming the cost leader with excellence in quality performance and on time delivery, while at the same time mindful of safety and environmental concerns.</p>
       <p class="paragraph">With our philosophy, values, and mission firmly in place, we are targeting controlled growth and sustained profitability. Our responsibility to our clients, our human resource, and shareholders remain to be the priority of our business and is defined by our corporate values.</p>
    </div>
    <div>
-      
+
+   </div>
+</div>
+<div class="text-white bg-primary-green md:p-14">
+   <h1 class="text-center text-5xl xl:mb-10">SERVICES</br></h1>
+
+   <div class="p-4 xl:p-12 flex flex-col md:flex-row md:flex-wrap gap-8 md:gap-0">
+      <div class="mb-4 basis-1/2 xl:basis-1/3">
+         <h1 class="text-3xl mb-4">
+            Civil Works
+         </h1>
+         <ul class="text-lg">
+            <li class="ml-4">➤ Roads, Bridges and Viaduct</li>
+            <li class="ml-4">➤ Power Plant, Dam and Spill Way</li>
+            <li class="ml-4">➤ Airport, Ports and Railways</li>
+            <li class="ml-4">➤ Industrial Waste Water Treatment and Incineration</li>
+            <li class="ml-4">➤ Slope Protection and Retaining Walls</li>
+            <li class="ml-4">➤ Cistern Tank, Surge  tank  and Swimming Pool</li>
+         </ul>
+      </div>
+      <div class="mb-4 basis-1/2 xl:basis-1/3">
+         <h1 class="text-3xl mb-4">
+            Deep Foundation Works
+         </h1>
+         <ul class="text-lg">
+            <li class="ml-4">➤ Concrete Pile Driving</li>
+            <li class="ml-4">➤ Bored Pile Driving</li>
+            <li class="ml-4">➤ Sheet Pile Driving</li>
+         </ul>
+      </div>
+      <div class="mb-4 basis-1/2 xl:basis-1/3">
+         <h1 class=" text-3xl mb-4">
+            Architectural Works
+         </h1>
+         <ul class="text-lg">
+            <li class="ml-4">➤ Construction of Commercial and Residential Building</li>
+            <li class="ml-4">➤ Warehouse  and Industrial Building</li>
+            <li class="ml-4">➤ Shopping Malls and Sports Complex</li>
+            <li class="ml-4">➤ High Rise Building Condominium and Hotels</li>
+            <li class="ml-4">➤ Poultry and School Building</li>
+            <li class="ml-4">➤ Expansion and Renovation Works</li>
+            <li class="ml-4">➤ Substations, Switchgears and Steel Towers</li>
+            <li class="ml-4">➤ Structure Renovations</li>
+            <li class="ml-4">➤ Expansion Projects</li>
+         </ul>
+      </div>
    </div>
 </div>
 <?php
@@ -35,6 +80,7 @@ require_once "includes/scripts.php";
    // document.body.style.backgroundSize = "cover";
    // document.body.style.backgroundAttachment = "fixed";
    document.body.style.background = "transparent";
+   document.body.className = "overflow-x-hidden"
 </script>
 </body>
 
