766169a2d5dcbd03b6286572e8048df04c17d15b
jerud-o <ocampo.jerud.2001@gmail.com>
2022-07-23 14:19:50 +0800

    temporary database

--- procedures/config.php
+++ procedures/config.php
@@ -1,11 +1,12 @@
 <?php
 
+// PLEASE HUWAG.
 function createConnection(
-    string $db_host = "192.168.1.86",
-    string $database = "brulife",
+    string $db_host = "ucc-csd.tech",
+    string $database = "jerud_brulife",
     string $charset = "utf8mb4",
-    string $db_username = "root",
-    string $db_password = "",
+    string $db_username = "jerud",
+    string $db_password = "7NnhGtEg_KjFKn0G",
     array $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
