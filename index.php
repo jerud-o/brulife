0170dac6b0a928e8b24402cfafa4015f56619aa8
jerud-o <ocampo.jerud.2001@gmail.com>
2022-07-26 09:23:24 +0800

    working SMTP

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
 define("APP_ROOT", "/" . basename(__DIR__) . "/");
 $uri = explode("/", $_SERVER["REQUEST_URI"]);
 
@@ -145,27 +148,28 @@ switch (true) {
                     
                 }
             }
-
+            
             if (count($error) === 0) {
                 $mail = new PHPMailer(true);
 
                 try {
                     //Server settings
-                    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                    $mail->isSMTP();                                            //Send using SMTP
-                    $mail->Host       = "smtp.gmail.com";                       //Set the SMTP server to send through
-                    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                    $mail->Username   = 'csdmail.north@gmail.com';                     //SMTP username
-                    $mail->Password   = 'rcgWrk03IKM6';                               //SMTP password
-                    $mail->SMTPSecure   = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-                    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                    $mail->isSMTP();
+                    $mail->Host       = "smtp.gmail.com";
+                    $mail->SMTPAuth   = true;
+                    $mail->Username   = 'csdmail.north@gmail.com';
+                    $mail->Password   = 'vaonblwwblocomdu';
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                    $mail->Port       = 587;
 
                     //Recipients
                     $mail->setFrom("csdmail.north@gmail.com", "CSD e-MAIL");
                     $mail->addReplyTo("csdmail.north@gmail.com");
+                    $mail->addAddress($_POST['email']);
 
                     //Content
-                    $mail->isHTML(true);                                  //Set email format to HTML
+                    $mail->isHTML(true);
                     $mail->Subject = 'Here is the subject';
                     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
                     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
@@ -177,6 +181,7 @@ switch (true) {
                 }
             }
         }
+
         require_once "templates/contact.php";
         break;
     case str_starts_with($uri[2], "test"): {
@@ -195,9 +200,9 @@ function isEmailValid($email)
     $findDots = str_contains($email, "..");
     $isValid = (
         !$checkStart
-        && $checkLen
-        && $findAt === 1
-        && $findDots === 0
+        || $checkLen
+        || $findAt === 1
+        || $findDots === 0
     );
     
     return $isValid;
