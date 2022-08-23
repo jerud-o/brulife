bcbef9491826caeb079fada4775a70313039f12b
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-08-23 06:49:53 +0000

    Move directors.css

--- templates/directors.php
+++ templates/directors.php
@@ -2,7 +2,7 @@
 
 <title>BRULife Directors</title>
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
-<link href="<?= APP_ROOT . 'assets/css/directors-output.css?' . uniqid() ?>" rel="stylesheet">
+<link href="<?= APP_ROOT . 'assets/css/directors.css?' . uniqid() ?>" rel="stylesheet">
 
 <?php require_once __DIR__ . "/includes/navbar.php"; ?>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -5,3 +5,4 @@ let mix = require('laravel-mix');
 mix.css('src/css/about.css', 'assets/css')
     .css('src/css/main.css', 'assets/css')
     .css('src/css/home.css', 'assets/css')
+    .css('src/css/directors.css', 'assets/css')
