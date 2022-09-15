b0586f2c3bfee588232b90897054e3638bf3dd0b
jerud-o <ocampo.jerud.2001@gmail.com>
2022-09-15 06:12:21 +0000

    updated paragraph

---
+++ templates/news.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius autem est ratione sint et eos. Voluptas ratione distinctio odit natus et quidem provident maiores. Voluptatum inventore blanditiis totam esse sunt.</p>
+</body>
+</html>
